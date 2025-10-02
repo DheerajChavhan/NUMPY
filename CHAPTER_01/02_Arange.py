65bbd0d97280355e9fc4cf8a7e15190c3e087ed4
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-10-03 00:06:23 +0530

    Add files via upload

---
+++ CHAPTER_01/02_Arange.py
@@ -0,0 +1,9 @@
+import numpy as np           # type: ignore
+
+arr=np.arange(1,10,2)
+print(arr)
+
+# Creating Identity Matrix
+identity_matrix=np.eye(3)
+print("Identity Matrix")
+print(identity_matrix)
