11edc833bf97e39b62e900cc46c2689406be3853
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-10-04 00:45:03 +0530

    Add files via upload

---
+++ CHAPTER_02/01_Shape&Size.py
@@ -0,0 +1,12 @@
+import numpy as np #type: ignore
+
+B=np.array([[1,2,3,4,5],
+            [6,7,8,9,10]])    
+  
+print("2_D Array")
+
+print(B.shape)      #Gives the shape of the array(2,5)
+print(B.size)       #Gives the size of the array(10)
+
+# Size=No. of Elements
+# Shape=(Rows,Columns) of Matrix
