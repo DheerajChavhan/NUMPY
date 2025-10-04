d652ee65a5c2f1bd73dd042ca3fe43b54b52f658
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-10-05 00:08:19 +0530

    Add files via upload

---
+++ CHAPTER_02/05_Aggregation.py
@@ -0,0 +1,10 @@
+import numpy as np #type:ignore
+
+arr=np.array([1,23,45,64,77])
+
+print(np.sum(arr))   #Total
+print(np.mean(arr))  #Average Value
+print(np.min(arr))   #Minimum Value
+print(np.max(arr))   #Maximum Value
+print(np.std(arr))   #Standard Deviation
+print(np.var(arr))   #Variance
