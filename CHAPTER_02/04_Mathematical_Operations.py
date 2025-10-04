5d51f43b7075c67b0b4e63be292a6f2afa1d7c7b
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-10-05 00:08:03 +0530

    Add files via upload

---
+++ CHAPTER_02/04_Mathematical_Operations.py
@@ -0,0 +1,7 @@
+import numpy as np #type:ignore
+
+arr=np.array([12,34,5,32])
+print(arr+5)                       #ADD 5 to every element in Array
+print(arr-5)                       #SUBTRACT 5 from every element in Array
+print(arr*5)                       #MULTIPLY 5 by every element in Array
+print(arr/3)                       #DIVIDE every element by 5 in Array
