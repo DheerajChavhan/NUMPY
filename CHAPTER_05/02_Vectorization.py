cb2784874a5cb95c05e4581c8eb639c89748268a
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-10-09 22:10:49 +0530

    Add files via upload

---
+++ CHAPTER_05/02_Vectorization.py
@@ -0,0 +1,8 @@
+import numpy as np #type: ignore
+
+arr=np.array([100,200,300,400])
+Multiplied=arr*3
+print(Multiplied)
+
+# Broadcasting: Expand smaller arrays to large
+# Vectorization: Apply any specific operation on entire array
